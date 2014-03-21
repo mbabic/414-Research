b6794fd576787cd76c09d7fb6a6b09bbd35fc813
Marcus <mkarpoff@ualberta.ca>
2014-03-21 11:35:12 -0600

    Template

---
+++ ResearchProgram/src/Analyser.java
@@ -0,0 +1,46 @@
+public class Analyser {
+
+	Analyser() {
+	}
+
+	/**
+	 * This will look for faces and return the coordinates of the faces
+	 */
+	public void detectFaces() {
+
+	}
+
+	/**
+	 * This will return the split video streams. It will take the original video
+	 * stream and the location of the faces as input. It will return two video
+	 * streams as a tuple. One will have the faces only the other will have
+	 * everything else.
+	 */
+	public void separateStreams() {
+
+	}
+
+	/**
+	 * This function will write onto of the frames that have facial data.
+	 */
+	public void blackOutFaces() {
+
+	}
+
+	/**
+	 * This function will do the foreground extraction. Having a standardized
+	 * size may be useful for simplification.
+	 */
+	public void extractVideo() {
+
+	}
+
+	/**
+	 * This function takes in the two split video streams and recombines them in
+	 * to one video stream.
+	 */
+	public void recombineVideo() {
+
+	}
+
+}
---
+++ ResearchProgram/src/Encryption.java
@@ -0,0 +1,20 @@
+public class Encryption {
+
+	public Encryption() {
+	}
+
+	/**
+	 * This should take in a video stream and return a encrypted video stream
+	 * For now Encrypt and Decrypt will do the exact same thing
+	 */
+	public void encrypt() {
+
+	}
+	
+	/**
+	 * This will take an encrypted video stream and return an video stream
+	 */
+	public void decrypt() {
+
+	}
+}
