fe013dcb392ea7e10a385855a803b2177401e173
mbabic <mbabic@ualberta.ca>
2014-03-30 11:04:57 -0600

    JUnit test folder created.

---
+++ ResearchProgram/tests/tests/FaceStreamTests.java
@@ -0,0 +1,34 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FaceStreamTests {
+
+	@Test
+	public void testFaceStream() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNextRectList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWriteToFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReadFromFile() {
+		fail("Not yet implemented");
+	}
+
+}
