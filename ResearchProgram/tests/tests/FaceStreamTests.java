7e7ae106cc0df3bfc6d36c42dbbec5b27f7e4649
mbabic <mbabic@ualberta.ca>
2014-03-30 15:58:07 -0600

    Tests update.

--- ResearchProgram/tests/tests/FaceStreamTests.java
+++ ResearchProgram/tests/tests/FaceStreamTests.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import Project.FaceStream;
+
 public class FaceStreamTests {
 
 	@Test
@@ -28,7 +30,9 @@ public class FaceStreamTests {
 
 	@Test
 	public void testReadFromFile() {
-		fail("Not yet implemented");
+		FaceStream fs = FaceStream.fromFile();
+		System.out.println(fs);
+		assert(1==1);
 	}
 
 }
