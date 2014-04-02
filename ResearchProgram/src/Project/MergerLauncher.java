3f3c990a2907bb0673a68eb1b7300998494b48b0
Marcus <mkarpoff@ualberta.ca>
2014-04-01 21:06:15 -0600

    Building the stream merger launcher

---
+++ ResearchProgram/src/Project/MergerLauncher.java
@@ -0,0 +1,17 @@
+package Project;
+
+import java.io.File;
+
+public class MergerLauncher {
+
+	public static void main(String[] args) {
+		UI gui = new UI();
+		File inb = new File("out/outb.yuv");
+		File inf = new File("out/outf.yuv");
+		Transmitter transmitter = new Transmitter();
+		Analyzer anlyzer;
+		
+		
+	}
+
+}
