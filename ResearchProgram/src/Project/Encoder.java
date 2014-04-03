77caa884e3d36662db7d4f4e756cce2a87e7ba8a
mbabic <mbabic@ualberta.ca>
2014-04-02 22:08:04 -0600

    Encoding update.

--- ResearchProgram/src/Project/Encoder.java
+++ ResearchProgram/src/Project/Encoder.java
@@ -1,10 +1,56 @@
 package Project;
 
 /**
- * Class responsible for receiving 
- * @author Marko
+ * Class responsible for receiving a 
+ * @author Marko Babic, Marcus Karpoff
  *
  */
 public class Encoder {
 
+	/** Width of a frame in the video to be encoded. */
+	private int _imgHeight;
+	
+	/** Height of a frame in the video to be encoded. */
+	private int _imgWidth;
+	
+	/** Frame rate of the video to be encoded. */
+	private int _fps;
+	
+	/** The number of frames in the video to be encoded. */
+	private int _frames;
+	
+	
+	public Encoder() {
+		_imgWidth = Settings.WIDTH;
+		_imgHeight = Settings.HEIGHT;
+		_fps = Settings.DEFAULT_FPS;
+
+	}
+	
+	public Encoder(int imgWidth, int imgHeight, int fps, int frames) {
+		if (imgWidth > 0) {
+			_imgWidth = imgWidth;
+		} else {
+			_imgWidth = Settings.WIDTH;
+		}
+		
+		if (imgHeight > 0) {
+			_imgHeight = imgHeight;
+		} else {
+			_imgHeight = Settings.HEIGHT;
+		}
+		
+		if (fps > 0) {
+			_fps = fps;
+		} else {
+			_fps = Settings.DEFAULT_FPS;
+		}
+		
+		if (frames > 0) {
+			_frames = frames;
+		} else {
+			_frames = Settings.DEFAULT_FRAMES;
+		}
+	}
+	
 }
--- ResearchProgram/src/Project/Settings.java
+++ ResearchProgram/src/Project/Settings.java
@@ -11,11 +11,12 @@ public class Settings {
 	public static final int WIDTH = 400;
 	public static final int FRAMERATE = 10;
 	public static final String CLASSIFIER_DIR = "res/haarcascades/";
+	public static final int DEFAULT_FPS = 30;
+	public static final int DEFAULT_FRAMES = 10;
 	public static final String OUTF = "out/outf.avi";
 	public static final String OUTB = "out/outb.avi";
 	public static final String FACESTREAM_OUT = "out/face_stream.ser";
 	public static final String DEFAULT_PASSWORD = "password";
-	
 	public static final String FFMPEG = "libs/ffmpeg.exe"; 
 	public static final String TAPPENCODER = "libs/TAppEncoder.exe";
 	public static final String TAPPDECODER = "libs/TAppDecoder.exe";
