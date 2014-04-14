23347d040384a0c0e0b482164c31b1099c88a090
Marcus <mkarpoff@ualberta.ca>
2014-04-13 22:21:25 -0600

    Merged changes

--- ResearchProgram/src/Project/FaceStream.java
+++ ResearchProgram/src/Project/FaceStream.java
@@ -42,25 +42,15 @@ public class FaceStream implements java.io.Serializable {
 	/**
 	 * Width of the frame.
 	 */
-<<<<<<< HEAD
+
 	public int _imgWidth;
-	
+
 	/**
 	 * Height of the frame.
 	 */
+
 	public int _imgHeight;
-	
-=======
-	@SuppressWarnings("unused")
-	private int _imgWidth;
 
-	/**
-	 * Height of the frame.
-	 */
-	@SuppressWarnings("unused")
-	private int _imgHeight;
-
->>>>>>> origin/master
 	/**
 	 * Empty constructor.
 	 */
